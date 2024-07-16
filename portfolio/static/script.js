2417a8b701bfaf63b00943c051fb9cfe6bb28b8b
Sharad Sisodia <sharadsisodia222@gmail.com>
2024-07-16 09:09:08 +0000

    css, html and js complete with django

---
+++ portfolio/static/script.js
@@ -0,0 +1,10 @@
+// Smooth Scrolling for Anchor Links
+document.querySelectorAll('.header a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
